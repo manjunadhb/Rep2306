10baaa9d4882f72f61d9a09443f9fd228c860673
Sandeepkukkadapu <kukkadapusandeep7@gmail.com>
2023-10-16 14:43:49 +0530

    some changes are done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ function App() {
   return (
     <div className="App">
      
-    
+    <form>
+      <h1>Login form</h1>
+      <div>
+       <label>FirstName <b>*</b></label>
+        <input></input>
+      </div>
+      <div>
+       <label>LastName<b>*</b> </label>
+        <input></input>
+      </div>
+      <div>
+       <label>Email  <b>*</b></label>
+        <input></input>
+      </div>
+      <div>
+       <label>Password <b>*</b> </label>
+        <input></input>
+      </div>
+      <button type="button"> Login</button>
+    </form>
       
     </div>
   );
