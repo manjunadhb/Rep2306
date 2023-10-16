8a812513f321d5ccace3e20d30339bf243dc150a
Sandeepkukkadapu <kukkadapusandeep7@gmail.com>
2023-10-16 14:15:01 +0530

    some changes are done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Cloned Original Repositoy and added name of the User.</p>
-      <h1>Sandeep</h1>
+     
+    
       
     </div>
   );
